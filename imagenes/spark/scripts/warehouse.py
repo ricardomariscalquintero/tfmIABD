197987c3f447fff5743453b92b6d38ff7301e313
Ricardo Mariscal Quintero <ricardo.mariscal.quintero@iesfernandoaguilar.es>
2025-07-04 04:45:11 +0200

    Refinando ETL

--- imagenes/spark/scripts/warehouse.py
+++ imagenes/spark/scripts/warehouse.py
@@ -2,9 +2,10 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import (
     col, to_date, year, month, dayofmonth, date_format,
-    monotonically_increasing_id
+    row_number, trim, coalesce, lit
 )
 from pyspark.sql.utils import AnalysisException
+from pyspark.sql.window import Window
 from subprocess import call
 from datetime import datetime
 import argparse
@@ -51,11 +52,13 @@ dfFechas = df.select("FechaPartida").union(df.select("FechaEvento")).distinct() 
     .withColumn("anio", year("fecha")) \
     .withColumn("mes", month("fecha")) \
     .withColumn("dia", dayofmonth("fecha")) \
-    .withColumn("nombre_mes", date_format("fecha", "MMMM")) \
-    .withColumn("id_fecha", monotonically_increasing_id())
+    .withColumn("nombre_mes", date_format("fecha", "MMMM"))
+
+windowFecha = Window.orderBy("fecha")
 
 if modoCarga == "inicial":
     #Como es inicial, se escribe completamente la dimensión.
+    dfFechas = dfFechas.withColumn("id_fecha", row_number().over(windowFecha))
     dfFechas.write.mode("overwrite").saveAsTable("dim_fecha")
 else:
     #Mediante el left_anti join conseguimos detectar que fechas están en el nuevo
@@ -63,10 +66,10 @@ else:
     #un append a la dimensión fecha con los nuevos.
     #Utilizo alias n(nuevos) y e(existentes) para que sea sencillo trabajar con
     #los datos.
-    dfFechas.alias("n").join(
+    nuevasFechas = dfFechas.alias("n").join(
         spark.table("dim_fecha").alias("e"), on="fecha", how="left_anti"
-    ).withColumn("id_fecha", monotonically_increasing_id()) \
-     .write.mode("append").saveAsTable("dim_fecha")
+    ).withColumn("id_fecha", row_number().over(windowFecha))
+    nuevasFechas.write.mode("append").saveAsTable("dim_fecha")
 
 #=======================
 #Dimensión Jugador
@@ -80,7 +83,7 @@ white = df.select(
     col("WhiteGenderTitle").alias("genero_titulo"),
     col("WhiteHasTitle").alias("tiene_titulo"),
     col("WhiteFideId").alias("fide_id")
-)
+).filter(col("fide_id").isNotNull())
 
 black = df.select(
     col("Black").alias("nombre"),
@@ -90,54 +93,75 @@ black = df.select(
     col("BlackGenderTitle").alias("genero_titulo"),
     col("BlackHasTitle").alias("tiene_titulo"),
     col("BlackFideId").alias("fide_id")
-)
+).filter(col("fide_id").isNotNull())
 
 #Unimos los dos dataframes en uno que sería la dimensión de jugadores.
-dimJugador = white.union(black).dropDuplicates(["fide_id"]) \
-    .withColumn("id_jugador", monotonically_increasing_id())
+dimJugador = white.union(black).dropDuplicates(["fide_id"])
+
+windowJugador = Window.orderBy("fide_id")
 
 #Actuamos de igual forma que anteriormente.
 if modoCarga == "inicial":
+    dimJugador = dimJugador.withColumn("id_jugador", row_number().over(windowJugador))
     dimJugador.write.mode("overwrite").saveAsTable("dim_jugador")
 else:
-    dimJugador.alias("n").join(
-        spark.table("dim_jugador").alias("e"), on="fide_id", how="left_anti"
-    ).withColumn("id_jugador", monotonically_increasing_id()) \
-     .write.mode("append").saveAsTable("dim_jugador")
+    nuevosJugadores = dimJugador.alias("n").join(
+        spark.table("dim_jugador").alias("e"),
+        on="fide_id", how="left_anti"
+    ).withColumn("id_jugador", row_number().over(windowJugador))
+    nuevosJugadores.write.mode("append").saveAsTable("dim_jugador")
 
 #=======================
 #Dimensión Evento
 #=======================
 #Creamos el dataframe con los campos relacionados.
 #Además, borramos los duplicados.
-dimEvento = df.select("Event", "Ciudad", "PaisISO3", "Pais", "Latitud", "Longitud", "Site", "MatchType") \
-    .dropDuplicates() \
-    .withColumn("id_evento", monotonically_increasing_id())
+df = df.withColumn("Event", trim(col("Event")))
+
+#Seleccionamos directamente las columnas que nos interesan.
+dimEvento = df.na.drop(subset=["Event"]).select(
+    "Event",
+    "Ciudad",
+    "PaisISO3",
+    "Pais",
+    "Latitud",
+    "Longitud",
+    "Site",
+    "MatchType"
+).dropDuplicates()
+
+windowEvento = Window.orderBy("Event")
 
 if modoCarga == "inicial":
+    dimEvento = dimEvento.withColumn("id_evento", row_number().over(windowEvento))
     dimEvento.write.mode("overwrite").saveAsTable("dim_evento")
 else:
-    dimEvento.alias("n").join(
-        spark.table("dim_evento").alias("e"),
-        on=["Event", "Ciudad", "PaisISO3", "Pais", "Latitud", "Longitud", "Site", "MatchType"],
-        how="left_anti"
-    ).withColumn("id_evento", monotonically_increasing_id()) \
-     .write.mode("append").saveAsTable("dim_evento")
+    nuevosEventos = dimEvento.alias("n").join(
+        spark.table("dim_evento").alias("e"), on="Event", how="left_anti"
+    ).withColumn("id_evento", row_number().over(windowEvento))
+    nuevosEventos.write.mode("append").saveAsTable("dim_evento")
 
 #=======================
 #Dimensión Apertura
 #=======================
-dimApertura = df.select("ECO", "Opening").dropDuplicates() \
-    .withColumn("id_apertura", monotonically_increasing_id())
+#Realizamos una limpieza de los campos para evitar los errores al realizar el join.
+df = df.withColumn("ECO", trim(coalesce(col("ECO"), lit("DESC")))) \
+       .withColumn("Opening", trim(coalesce(col("Opening"), lit("Desconocida"))))
+
+#Eliminamos las filas con duplicados.
+dimApertura = df.select("ECO", "Opening").dropDuplicates(["ECO", "Opening"])
+
+windowApertura = Window.orderBy("ECO", "Opening")
 
 if modoCarga == "inicial":
+    dimApertura = dimApertura.withColumn("id_apertura", row_number().over(windowApertura))
     dimApertura.write.mode("overwrite").saveAsTable("dim_apertura")
 else:
-    dimApertura.alias("n").join(
+    nuevasAperturas = dimApertura.alias("n").join(
         spark.table("dim_apertura").alias("e"),
         on=["ECO", "Opening"], how="left_anti"
-    ).withColumn("id_apertura", monotonically_increasing_id()) \
-     .write.mode("append").saveAsTable("dim_apertura")
+    ).withColumn("id_apertura", row_number().over(windowApertura))
+    nuevasAperturas.write.mode("append").saveAsTable("dim_apertura")
 
 #=======================
 #Hecho Partida
@@ -148,6 +172,7 @@ dimEvento   = spark.table("dim_evento")
 dimApertura = spark.table("dim_apertura")
 
 hechoPartida = df \
+    .filter(col("WhiteFideId").isNotNull() & col("BlackFideId").isNotNull()) \
     .join(dimJugador.select("id_jugador", col("fide_id").alias("WhiteFideId")),
           on="WhiteFideId", how="left") \
     .withColumnRenamed("id_jugador", "id_jugador_blanco") \
@@ -160,28 +185,27 @@ hechoPartida = df \
     .join(dimFecha.withColumnRenamed("fecha", "FechaEvento"),
           on="FechaEvento", how="left") \
     .withColumnRenamed("id_fecha", "id_fecha_evento") \
-    .join(dimEvento,
-          on=["Event", "Ciudad", "PaisISO3", "Pais", "Latitud", "Longitud", "Site", "MatchType"],
-          how="left") \
-    .join(dimApertura,
-          on=["ECO", "Opening"],
-          how="left") \
+    .join(dimEvento, on="Event", how="left") \
+    .join(dimApertura, on=["ECO", "Opening"], how="left") \
     .select(
-        monotonically_increasing_id().alias("id_partida"),
         "id_jugador_blanco", "id_jugador_negro",
         "id_fecha_partida", "id_fecha_evento",
         "id_evento", "id_apertura",
         "ResultadoBinario", "Ganador", "DiferenciaELO", "Round"
     )
 
+windowPartida = Window.orderBy("id_evento", "id_fecha_partida", "Round")
+hechoPartida = hechoPartida.withColumn("id_partida", row_number().over(windowPartida))
+
 if modoCarga == "inicial":
     hechoPartida.write.mode("overwrite").saveAsTable("hecho_partida")
 else:
-    hechoPartida.alias("n").join(
+    nuevasPartidas = hechoPartida.alias("n").join(
         spark.table("hecho_partida").alias("e"),
         on=["id_jugador_blanco", "id_jugador_negro", "id_fecha_partida", "id_evento", "Round"],
         how="left_anti"
-    ).write.mode("append").saveAsTable("hecho_partida")
+    )
+    nuevasPartidas.write.mode("append").saveAsTable("hecho_partida")
 
 #=======================
 #Renombramos el CSV procesado para que no se vuelva a procesar
